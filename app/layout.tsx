28f4845c9b8b826208cce275d26b4623aece1fa8
hectorguedea <hectorguedea@gmail.com>
2024-04-17 13:56:41 -0600

    Metadata on pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Mr. PopUp Dashboard',
+    default: 'Mr. PopUp Dashboard',
+  },
+  description: 'The official Mr. PopUp Dashboard',
+  metadataBase: new URL('https://nextjs-dashboard-one-wheat-88.vercel.app'),
+};
 
 export default function RootLayout({
   children,
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,11 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
+import { Metadata } from 'next';
  
+export const metadata: Metadata = {
+  title:  'Login',
+  description: 'Access to Mr. PopUp Dashboard',
+};
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
